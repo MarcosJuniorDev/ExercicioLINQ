16b2e1d1628e9d47b213ed396bed52c5b629d5ed
Marcos Junior Oliveira <marcosjoliveiradev@gmail.com>
2023-05-16 17:21:30 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioPropostoLINQ/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace ExercicioPropostoLINQ.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
